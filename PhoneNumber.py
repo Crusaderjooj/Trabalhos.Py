5cd9d63ec35b5bfb32aa21864c5c3713676ff65d
Joaquim_Jooj <jocahj20@gmail.com>
2025-04-22 19:38:47 -0300

    Create PhoneNumber.py
    In this one i was requested to make a system where the user could add phone numbers to a dictionary.

---
+++ PhoneNumber.py
@@ -0,0 +1,11 @@
+contato = []
+numero = []
+
+tabela = {}
+
+for i in range (3):
+    contato = input("Digite um nome: ")
+    numero = input("Digite um numero de telefone: ")
+    tabela[contato] = numero
+print (tabela)
+
